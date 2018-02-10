ecc65b4389aa0183595c213399f1a609f522ce74
faizalrazak <mohamadfaizalrazak@gmail.com>
2018-02-10 13:27:53 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Printer } from '@ionic-native/printer';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -23,6 +24,7 @@ import { HomePage } from '../pages/home/home';
   ],
   providers: [
     StatusBar,
+    Printer,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Printer, PrintOptions } from '@ionic-native/printer';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public printer:Printer) {
 
+  }
+
+  print(){
+  	this.printer.isAvailable().then(function(){
+
+        this.printer.print("https://www.techiediaries.com").then(function(){
+            alert("printing done successfully !");
+        },function(){
+            alert("Error while printing !");
+    	});
+    }, 
+    function(){
+        alert('Error : printing is unavailable on your device ');
+    });
   }
 
 }
